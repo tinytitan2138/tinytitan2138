bc05c05da221334fd3673944d742845cc77c7536
tinytitan2138 <99772191+tinytitan2138@users.noreply.github.com>
2022-03-29 17:47:26 -0700

    Basic program which assigns a workout routine.
    Uses 1 function, probably could've made things much simpler and much more advanced.

---
+++ Question copy.py
@@ -0,0 +1,3 @@
+class Questions:
+    def __init__(self, prompt):
+        self.prompt = prompt
--- main copy.py
+++ main copy.py
@@ -1,100 +1,115 @@
-from threading import Thread, Lock, current_thread
-from multiprocessing import Process
-import os
-import time
-from queue import Queue
-def square_numbers():
-    for i in range(100):
-        i * i
-        time.sleep(0.1)
-
-processes = []
-num_processes = os.cpu_count()
-# Create processes
-for i in range(num_processes):
-    p = Process(target=square_numbers)  # If fuction has argument pass in args
-# Start
-for p in processes:
-    p.start()
-# Join
-for p in processes:
-    p.join()
-print('End main')
-
-threads = []
-num_threads = 10
-# Creates thread.
-for i in range(num_threads):
-    t = Thread(target=square_numbers)
-    threads.append(t)
-# Start
-for t in threads:
-    t.start()
-# Join
-for t in threads:
-    t.join()
-
-database_value = 0
-
-def increase(lock):
-    global database_value
-    with lock:
-        local_copy = database_value
-        # Processing
-        local_copy += 1
-        time.sleep(0.1)
-        database_value = local_copy
-
-
-
-
-if __name__ == "__main__":
-    lock = Lock()
-    print('Start value', database_value)
-    thread1 = Thread(target=increase, args=(lock,))
-    thread2 = Thread(target=increase, args=(lock,))
-
-    thread1.start()
-    thread2.start()
-
-    thread1.join()
-    thread2.join()
-
-    print('end value', database_value)
-    print('end main')
-
-q = Queue()
-q.put(1)
-q.put(2)
-q.put(3)
-# 3 2 1 -->
-first = q.get()
-print(first)
-q.task_done()
-print('end main')
-q.join()
-
-def worker(q):
-    while True:
-        value = q.get()
-        # Processing...
-        print(f'in {current_thread().name} got {value}')
-        q.task_done()
+from Question import Questions
 
 
 
 
 
-if __name__ == "__main__":
-    q = Queue()
-    num_threads = 10
-    for i in range(num_threads):
-        thread = Thread(target=worker, args=(q,))
-        thread.daemon=True
-        thread.start()
-    for i in range(1, 21):
-        q.put(i)
-    q.join()
-    print('end main')
+
+question = [
+    "How old are you?\n\n",
+    "What is your fitness goal?\n(a)weight loss\n(b)aesthetic appearence\n(c)strength gain\n\n",
+    "What is your gender?\n(a)M\n(b)F\n\n"
+]
+
+questions = [
+    Questions(question[0]),
+    Questions(question[1]),
+    Questions(question[2]),
+]
+
+
+def get_routine(questions):
+    range1 = range(0, 13)
+    range2 = range(70, 121)
+    range3 = range(13, 18)
+    range4 = range(50, 69)
+    range5 = range(18, 25)
+    range6 = range(35, 49)
+    range7 = range(25, 34)
+
+    answer1 = input(questions[0].prompt)
+    intensity1 = False
+    intensity2 = False
+    intensity3 = False
+    wl = False
+    a = False
+    s = False
+    m = False
+    f = False
+
+    if int(answer1) in range1 or int(answer1) in range2:
+        print("You are not of age to workout.")
+        return
+    elif int(answer1) in range3 or int(answer1) in range4:
+        intensity1 = True
+    elif int(answer1) in range5 or int(answer1) in range6:
+        intensity2 = True
+    elif int(answer1) in range7:
+        intensity3 = True
+    else:
+        print("Accurate age not given")
+        return
+    answer2 = input(questions[1].prompt)
+    answer2 = answer2.upper()
+    if answer2 == "A":
+        wl = True
+    elif answer2 == "B":
+        a = True
+    elif answer2 == "C":
+        s = True
+    else:
+        print("Valid goal not given.")
+        return
+    answer3 = input(questions[2].prompt)
+    answer3 = answer3.upper()
+    if answer3 == "A":
+        m = True
+    elif answer3 == "B":
+        f = True
+    else:
+        print("Valid gender not given")
+        return
+    if intensity1 and wl and m:
+        print('Jog for 30 minutes a day, each day, and diet.')
+        return
+    elif intensity1 and a and m:
+        print('Do pushups, situps, and squats, 2 sets of reps to failure, every other day.')
+    elif intensity1 and s and m:
+        print('Do 2 sets of reps to failure, curls, shoulder presses, and dumbbell benchpress every other day.')
+    elif intensity1 and wl and f:
+        print('Jog for 20 minutes a day, each day, and diet.')
+    elif intensity1 and a and f:
+        print('Do girl pushups, situps, and squats, 2 sets of reps to failure, each day.')
+    elif intensity1 and s and f:
+        print('Do very light curls and shoulder presses for 2 sets of 10 every other day')
+    elif intensity2 and wl and m:
+        print('Jog for 1 hr and 30 minutes each day and diet.')
+    elif intensity2 and a and m:
+        print('Do 3 sets of reps to failure of pushups, situps, squats, and chinups eachday')
+    elif intensity2 and s and m:
+        print('Do 3 sets of 15 of curls, shoulder presses, and benchpress one day. The next day rest. The following 3 '
+              'sets of 15 squats and lunges')
+    elif intensity2 and wl and f:
+        print('Jog for 1 hour and 10 minutes each day, and diet.')
+    elif intensity2 and a and f:
+        print('Run at least 3 miles a day, and do 5 sets of reps to failure of squats and situps.')
+    elif intensity2 and s and f:
+        print('Do benchpress, curls, and deadlifts one day, rest for 2 day, and do shoulder presses and squats the '
+              'following day.')
+    elif intensity3 and wl and m:
+        print('Run for 8 miles a day minimum.')
+    elif intensity3 and a and m:
+        print('5 sets of reps to failure, pushups, situps, pullups, squats, and dips, everyday.')
+    elif intensity3 and s and m:
+        print('5 sets of reps to failure, benchpress, shoulder press, curls, and dumbbell squats each day.')
+    elif intensity3 and wl and f:
+        print('Run for 5 miles a day minimum')
+    elif intensity3 and a and f:
+        print('5 sets of reps to failure, squats, lunges, and situps.')
+    elif intensity3 and s and f:
+        print('5 sets of reps to failure, squats, lunges, and situps')
+    return
+
+get_routine(questions)
 
 
